5ae79ac83760dcdc838551403b37153e72a882e1
Goida <gaben@gmail.com>
2025-09-22 21:18:37 +0300

    Почти доделали левую часть

--- src/app/left-container/left-container.component.ts
+++ src/app/left-container/left-container.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome'
+import { faMagnifyingGlass, faLocationDot } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-left-container',
-  imports: [],
+  imports: [FaIconComponent],
   templateUrl: './left-container.component.html',
   styleUrl: './left-container.component.css'
 })
-export class LeftContainerComponent {
 
+export class LeftContainerComponent {
+  faMagnifyingGlass:any = faMagnifyingGlass
+  faLocationDot:any = faLocationDot
 }
