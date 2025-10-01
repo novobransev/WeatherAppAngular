230e939a168f1c209b7663de9073ad741d147335
Goida <gaben@gmail.com>
2025-10-01 21:16:55 +0300

    Сервисы

--- src/app/Services/weather.service.ts
+++ src/app/Services/weather.service.ts
@@ -9,22 +9,68 @@ import { TodayHightlight } from '../Models/TodaysHightlights';
 import { Observable } from 'rxjs';
 import { get } from 'http';
 import { EnvironmentVariables } from '../Environment/EnvironmentVariables';
+import { response } from 'express';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
   cityName:string = "Moscow";
+  language:string = "en-US";
+  date:string = '20251002'
+  units:string = 'm'
+
+  currentTime:Date = new Date();
 
   locationDetails?: LocationDetails;
   weatherDetails?: WeatherDetails;
 
-  temperatureData?: TemperatureData;
+  temperatureData: TemperatureData;
   todayData?: TodayData;
   weekData?: WeekData;
   todaysHightlights?: TodayHightlight;
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) { 
+    this.getData();
+  }
+
+  getSummaryImage(summary:string):string{
+    var cloudySunny = "https://cdn-icons-png.flaticon.com/128/1146/1146869.png"
+    var rainSunny = "https://cdn-icons-png.flaticon.com/128/12607/12607703.png"
+    var windy = "https://cdn-icons-png.flaticon.com/128/2044/2044028.png"
+    var sunny = "https://cdn-icons-png.flaticon.com/128/9231/9231728.png"
+    var rainy = "https://cdn-icons-png.flaticon.com/128/4724/4724094.png"
+
+    if(String(summary).includes("Partly Cloudy") || String(summary).includes("P Cloudy")) return cloudySunny
+    else if(String(summary).includes("Partly Rainy") || String(summary).includes("P Rainy")) return rainSunny
+    else if(String(summary).includes("wind")) return windy
+    else if(String(summary).includes("rain")) return rainy
+    else if(String(summary).includes("Sun")) return sunny
+
+    return cloudySunny
+  }
+
+  prepareData():void {
+    this.currentTime = new Date();
+    this.temperatureData.day = this.weatherDetails['v3-wx-observations-current'].dayOfWeek;
+    this.temperatureData.time = `${String(this.currentTime.getHours()).padStart(2, '0')}:${String(this.currentTime.getMinutes()).padStart(2, '0')}`
+    this.temperatureData.temperature = this.weatherDetails['v3-wx-observations-current'].temperature;
+    this.temperatureData.location = `${this.locationDetails.location.city[0]},${this.locationDetails.location.country[0]}`
+    this.temperatureData.rainPercent = this.weatherDetails['v3-wx-observations-current'].precip24Hour;
+    this.temperatureData.SummaryPhrase = this.weatherDetails['v3-wx-observations-current'].wxPhraseShort;
+    this.temperatureData.summaryImage = this.getSummaryImage(this.temperatureData.SummaryPhrase)
+  }
+
+  getLocationDetails(cityName: string, language:string):Observable<LocationDetails>{
+    return this.httpClient.get<LocationDetails>(EnvironmentVariables.weatherApiLocationBaseURL, {
+      headers: new HttpHeaders()
+        .set(EnvironmentVariables.xRapidApiKeyName, EnvironmentVariables.xRapidApiKeyValue)
+        .set(EnvironmentVariables.xRapidApiHostName, EnvironmentVariables.xRapidApiHostValue),
+      params: new HttpParams()
+        .set('query', cityName)
+        .set('language', language)
+    })
+  }
 
   getWeatherReport(date: string, latitude: number, longitude: number, language: string, units: string):Observable<WeatherDetails> {
     return this.httpClient.get<WeatherDetails>(EnvironmentVariables.weatherApiForecastBaseURL, {
@@ -41,6 +87,28 @@ export class WeatherService {
   }
 
   getData() {
+    var latitude = 0;
+    var longitude = 0;
+    this.getLocationDetails(this.cityName, this.language).subscribe({
+      next:(response)=> {
+        this.locationDetails = response;
+        latitude = this.locationDetails?.location.latitude[0];
+        longitude = this.locationDetails?.location.longitude[0];
 
+        this.getWeatherReport(
+          this.date,
+          latitude,
+          longitude,
+          this.language,
+          this.units
+        ).subscribe({
+          next:(response) => {
+            this.weatherDetails = response;
+
+            this.prepareData();
+          }
+        })
+      }
+    })
   }
 }
--- src/app/left-container/left-container.component.ts
+++ src/app/left-container/left-container.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome'
 import { faMagnifyingGlass, faLocationDot, faCloud, faCloudRain } from '@fortawesome/free-solid-svg-icons'
+import { WeatherService } from '../Services/weather.service';
 
 @Component({
   selector: 'app-left-container',
   imports: [FaIconComponent],
   templateUrl: './left-container.component.html',
-  styleUrl: './left-container.component.css'
+  styleUrl: './left-container.component.css',
+  providers: [WeatherService]
 })
 
 export class LeftContainerComponent {
@@ -15,4 +17,6 @@ export class LeftContainerComponent {
 
   faCloud:any = faCloud;
   faCloudRain:any = faCloudRain;
+
+  constructor(public weatherService:WeatherService){}
 }
